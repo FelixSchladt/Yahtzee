79703cf1d9a334c82ff772553d70f535ae23cb05
Thomas Gingele <thomas@gingele.de>
2022-04-06 19:39:15 +0200

    Add tests for quadrupel rule

--- src/test_rule_generator.py
+++ src/test_rule_generator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # pylint: disable=C
 from unittest import TestCase
-from rule_generator import triplet
+from rule_generator import triplet,\
+                           quadrupel
 
 
 class TestRuleGenerator(TestCase):
@@ -10,3 +11,10 @@ class TestRuleGenerator(TestCase):
         self.assertEqual(triplet([5, 2, 3, 5, 5]), (True, 15))
         self.assertEqual(triplet([1, 2, 3, 4, 5]), (False, 0))
         self.assertEqual(triplet([1, 1, 1, 1, 6]), (False, 0))
+
+    def test_quadrupel(self):
+        self.assertEqual(quadrupel([1, 1, 1, 1, 6]), (True, 4))
+        self.assertEqual(quadrupel([5, 2, 5, 5, 5]), (True, 20))
+        self.assertEqual(quadrupel([1, 2, 3, 4, 5]), (False, 0))
+        self.assertEqual(quadrupel([1, 1, 1, 1, 1]), (False, 0))
+        self.assertEqual(quadrupel([1, 1, 1, 2, 2]), (False, 0))
