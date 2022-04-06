70f1309adbe300128e0b6efd7a1f61679f815504
Thomas Gingele <thomas@gingele.de>
2022-04-06 19:27:05 +0200

    Update test file

--- src/test_rule_genererator.py
+++ src/test_rule_genererator.py
@@ -7,7 +7,3 @@ from rule_generator import triplet
 class TestRuleGenerator(TestCase):
     def test_triplet(self):
         self.assertEqual(triplet([1, 1, 1, 4, 5]), (True, 3))
-
-if __name__ == "__main__":
-    test = TestRuleGenerator()
-    test.test_triplet()
