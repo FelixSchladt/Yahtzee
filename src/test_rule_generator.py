041c3cde4aa2d7c97eae2cc67d19d07889c5d3b6
Thomas Gingele <thomas@gingele.de>
2022-04-06 19:32:39 +0200

    Update tests

--- src/test_rule_generator.py
+++ src/test_rule_generator.py
@@ -7,3 +7,6 @@ from rule_generator import triplet
 class TestRuleGenerator(TestCase):
     def test_triplet(self):
         self.assertEqual(triplet([1, 1, 1, 4, 5]), (True, 3))
+        self.assertEqual(triplet([5, 2, 3, 5, 5]), (True, 15))
+        self.assertEqual(triplet([1, 2, 3, 4, 5]), (False, 0))
+        self.assertEqual(triplet([1, 1, 1, 1, 6]), (False, 0))
