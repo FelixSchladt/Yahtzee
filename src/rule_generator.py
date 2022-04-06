5b37efd8b14d3148d52a8bcbce24e9f760c6f78b
Thomas Gingele <thomas@gingele.de>
2022-04-06 19:15:24 +0200

    Add triplet rule

---
+++ src/rule_generator.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+def check_rules(throws: []) -> int:
+    pass
+
+def check_upper_block() -> int:
+    pass
+
+def check_lower_block() -> int:
+    pass
+
+#
+# LOWER BLOCK RULES
+#
+
+def triplet(throws: []) -> (bool, int):
+    '''This method checks whether the eyes fullfill
+       the requirements of a triplet and returns the score
+
+       :param throws: the throws that will be checked
+       :returns: whether the rule was fullfilled or not as well as the potential score
+    '''
+    equal: int = 0
+    score: int = 0
+    is_rule = False
+
+    for i in range(1,6):
+        for eyes in throws:
+            if eyes == i:
+                equal += 1
+
+        if equal == 3:
+            score = i*3
+            is_rule = True
+            break
+
+        equal = 0
+
+    return (is_rule, score)
+
+
+def quadrupel() -> int:
+    # return Sum of eyes
+    pass
+
+
+def full_house() -> int:
+    return 25
+
+
+def small_road() -> int:
+    return 30
+
+
+def big_road() -> int:
+    return 40
+
+
+def yahtzee() -> int:
+    return 50
+
+
+def chance() -> int:
+    # return sum of all eyes
+    pass
