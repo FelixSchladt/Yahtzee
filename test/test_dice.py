7f28b2f23df46caa2a0e2cd367670c203d6ff642
Thomas Gingele <thomas@gingele.de>
2022-04-15 15:02:02 +0200

    Initial Add

---
+++ test/test_dice.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+# pylint: disable=C
+
+# Copyright 2022 Ginthom (https://github.com/GinThom)
+
+from unittest import TestCase
+
+from src.dices import dice, get_dices
+
+
+class TestRuleGenerator(TestCase):
+    def test_get_dices(self):
+        pass
+
+    def test_dice(self):
+        pass
