bf658949a120c025d25819e4c84c8a0a436b88a8
EisenmannLukas <lukaseisele.01@gmail.com>
2022-04-16 17:50:42 +0200

    add term_size function for windows

---
+++ File_Handler.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+
+def save_score(json_dict):
+    # json_dict = json_dict.to_json()  //Falls es ein Objekt ist
+    acess = "w" if os.path.isfile(f"score_yahtzee.json") else "x"
+    with open(f"score_yahtzee.json", acess, encoding="UTF-8") as f:
+        json.dump(json_dict, f)
+
+
+def main():
+    json_dict = {"Snickers": 1.2,
+                 "Mars": 1.3,
+                 "Nuts": 2,
+                 "Lindt": 4,
+                 "Milka": 2.2,
+                 "Alpenmilch": 3, }
+    save_score(json_dict)
