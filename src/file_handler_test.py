601a31d12f4b2ac260127723cd2ddde600f77418
EisenmannLukas <lukaseisele.01@gmail.com>
2022-04-18 17:26:19 +0200

    add src dir

---
+++ src/file_handler_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from unittest.mock import patch, mock_open
+
+import file_handler
+
+
+class FileHandler(unittest.TestCase):
+
+    def test_writetofile(tmpdir):
+        open_mock = mock_open()
+        with patch("file_handler.open", open_mock, create=True):
+            file_handler.save_score('test-data')
+
+        open_mock.assert_called_with("score_yahtzee.json", "w", encoding="UTF-8")
+        open_mock.return_value.write.assert_called_once_with('"test-data"')
+
+
+if __name__ == '__main__':
+    unittest.main()
