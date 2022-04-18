16376516f4f9e23ae840b0e6c5bc4d23e95b4fcc
EisenmannLukas <lukaseisele.01@gmail.com>
2022-04-18 12:49:14 +0200

    remove pylint warnings

--- File_Handler.py
+++ File_Handler.py
@@ -4,16 +4,24 @@ from exeptions import EmptyFileError
 
 
 def save_score(json_dict):
+    """
+    This methods saves the score in a json file
+    :rtype: object
+    """
     # json_dict = json_dict.to_json()           //Falls es ein Objekt ist
-    acess = "w" if os.path.isfile(f"score_yahtzee.json") else "x"
-    with open(f"score_yahtzee.json", acess, encoding="UTF-8") as f:
-        json.dump(json_dict, f)
+    acess = "w" if os.path.isfile("score_yahtzee.json") else "x"
+    with open("score_yahtzee.json", acess, encoding="UTF-8") as file:
+        json.dump(json_dict, file)
 
 
 def get_score():
+    """
+    This mehtod gets the score from a json file.
+    :rtype: object
+    """
     if os.stat("score_yahtzee.json").st_size > 0:
 
-        with open(f"score_yahtzee.json", "r", encoding="UTF-8") as file:
+        with open("score_yahtzee.json", "r", encoding="UTF-8") as file:
             file_content = json.load(file)
             if not file_content:
                 print("no data in file")
@@ -21,19 +29,3 @@ def get_score():
         print(file_content)
     else:
         raise EmptyFileError("File is empty")
-
-
-def main():
-    # json_dict = {"Snickers": 1.2,
-    #            "Mars": 1.3,
-    #           "Nuts": 2,
-    #          "Lindt": 4,
-    #         "Milka": 2.2,
-    #        "Alpenmilch": 3, }
-
-    # save_score(json_dict)
-    get_score()
-
-
-if __name__ == "__main__":
-    main()
