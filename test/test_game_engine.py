1bc6470778f46c5397f59b3bed88a7f4908c3b30
Thomas Gingele <thomas@gingele.de>
2022-04-24 20:38:46 +0200

    Add necessary test files

--- test/test_game_engine.py
+++ test/test_game_engine.py
@@ -4,6 +4,7 @@
 
 # pylint: disable=C
 
+import os
 from unittest import TestCase
 from json import JSONDecodeError
 from src.game_engine import GameEngine
@@ -15,11 +16,10 @@ class TestRuleGenerator(TestCase):
         self.correct_file = "./test/save_files/correct"
         self.faulty_json_file = "./test/save_files/faulty_format"
         self.corrupted_file = "./test/save_files/corrupted"
+        self.write_too_file = "./test/save_files/write_tests_here"
 
     def test_load_game_correct(self):
         self.engine.save_path = self.correct_file
-        # Check for some values to be correct as 
-        # checking for ALL of them would be overkill
         self.assertTrue(len(self.engine.players) == 2)
         self.assertTrue(len(self.engine.players[0].dices) == 5)
         self.assertTrue(self.engine.turns == 3)
@@ -34,3 +34,7 @@ class TestRuleGenerator(TestCase):
         # Error is dependent on what part of the file is corrupted
         self.assertRaises((ValueError, KeyError), self.engine.load_game)
 
+    def test_save_game(self):
+        self.engine.save_path = self.write_too_file
+        self.engine.save_game()
+        self.assertTrue(os.path.isfile(f"{self.write_too_file}.json"))
