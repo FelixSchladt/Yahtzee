5836288f11cd10fea310a5f07657cbf73d22d5c0
multiinside <jannikkuenstler@gmail.com>
2022-04-25 21:56:53 +0200

    added argparse in main.py

--- main.py
+++ main.py
@@ -7,6 +7,17 @@
 '''
 
 from src.game_engine import GameEngine
+import argparse
+
+def main():
+    parser_name = argparse.ArgumentParser(description='Set player names')
+    parser_name.add_argument('--name_one', type=str)
+    parser_name.add_argument('--name_two', type=str)
+    args_name = parser_name.parse_args()
+
+    parser_savefile = argparse.ArgumentParser(description='Load a savefile')
+    parser_savefile.add_argument('--savefile', type=int)
+    args_savefile = parser_savefile.parse_args()
 
 if __name__ == '__main__':
     GameEngine().run()
