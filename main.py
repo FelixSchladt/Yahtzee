df94393b48f85a7af4f2a5d931643aa2edbf9f66
Felix Schladt <felix.schladt@hensoldt-cyber.com>
2022-04-26 13:42:39 +0200

    working resize

--- main.py
+++ main.py
@@ -7,11 +7,25 @@
 '''
 
 import argparse
+import signal
+import sys
+
 from src.game_engine import GameEngine
+from src.exceptions import OutOfBoundsError
+from src.term_info import terminal
+
+def sigint(sig, frame):
+    """Callbac function for exiting game when SIGINT or SIGKILL
+    """
+    terminal().clear()
+    sys.exit(0)
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, sigint)
+
     parser = argparse.ArgumentParser(description='Basic settings for your Kniffel game')
     parser.add_argument('--savefile', type=str)
     parser.add_argument('--player-one', type=str)
     parser.add_argument('--player-two', type=str)
+
     GameEngine(*vars(parser.parse_args()).values()).run()
