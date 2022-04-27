7a1b41cd410391ee6a92c3b97a6ad858c5eaec7c
Felix Schladt <felix.schladt@hensoldt-cyber.com>
2022-04-27 11:48:24 +0200

    not working

--- test/test_tui_engine.py
+++ test/test_tui_engine.py
@@ -4,7 +4,7 @@
 # pylint: disable=C
 
 from unittest import TestCase, expectedFailure
-from src.tui_engine import TuiEngine
+from src.tui_engine import TuiEngine, category_table, OFFSET
 
 
 class TestTuiEngine(TestCase):
@@ -31,12 +31,17 @@ class TestTuiEngine(TestCase):
 
     def test_text(self):
         self.tui.reset_grid()
-        self.tui.text(0,0, "test", 3)
+        self.tui.text((0,0), "test", 3)
         self.assertEqual(self.tui._TuiEngine__grid[1][3], " ")
 
     @expectedFailure
     def test_OutOfBounds(self):
         self.tui.pixel(0, 9999999999999999999999)
+
+    def test_category_table(self):
+        category_table(self.tui)
+        self.assertEqual(self.tui._TuiEngine__grid[self.tui.display_width - OFFSET - 1 ][18], "P")
+
 
     def tearDown(self):
         pass
