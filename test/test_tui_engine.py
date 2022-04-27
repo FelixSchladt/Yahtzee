b0af0aeccc80a7165c49084ad0768c7d3bf1786a
Felix Schladt <felix.schladt@hensoldt-cyber.com>
2022-04-27 14:06:33 +0200

    fix test_tui_enigne.py

--- test/test_tui_engine.py
+++ test/test_tui_engine.py
@@ -56,12 +56,9 @@ class TestTuiEngine(TestCase):
     def test_log(self):
         log("Test")
         with open("tui_engine.log", "rb") as file:
-            try:
-                file.seek(-2, os.SEEK_END)
-                while file.read(1) != b'\n':
-                    file.seek(-2, os.SEEK_CUR)
-            except OSError:
-                file.seek(0)
+            file.seek(-2, os.SEEK_END)
+            while file.read(1) != b'\n':
+                file.seek(-2, os.SEEK_CUR)
             last_line = file.readline().decode()
         self.assertEqual(last_line, "Test\n")
 
